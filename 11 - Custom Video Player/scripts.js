cd18a06bd42541f9f269b14b077c5397e7aa050b
Tim Crisp <timothycrisp@gmail.com>
2023-02-06 14:03:04 -0600

    fix syntax

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -60,3 +60,5 @@ progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
+
+

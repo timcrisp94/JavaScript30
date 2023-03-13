2c881022fa78d78512663df0262b1b9ee4d12d14
Tim Crisp <timothycrisp@gmail.com>
2023-03-13 13:04:01 -0500

    add full screen capability to video player, begin readme

---
+++ 11 - Custom Video Player/readme.md
@@ -0,0 +1,15 @@
+Solution for Wes Bos's JavaScript 30 Day 11
+
+JavaScript is divided into three sections
+1. Get Elements
+2. Build Functions
+3. Add Event Listeners
+
+
+==togglePlay==
+```
+function togglePlay() {
+    const method = video.paused ? 'play' : 'pause'
+    video[method]()
+}
+```
--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -9,6 +9,9 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const fullScreenButton = player.querySelector('.fullscreen')
+
+let isFullScreen = false
 
 /*
 build out functions
@@ -43,6 +46,40 @@ function scrub(e) {
     const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration
     video.currentTime = scrubTime
 }
+
+function toggleFullScreen(e) {
+    if (isFullScreen) {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitCancelFullScreen) {
+            document.webkitCancelFullScreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        } else {
+            console.error('Unable to find a fullscreen exit method.');
+        }
+        console.log('removing fullscreen class');
+        } else {
+        if (player.requestFullscreen) {
+            player.requestFullscreen(); // standard
+        } else if (player.webkitRequestFullscreen) {
+            player.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+        } else if (player.mozRequestFullScreen) {
+            player.mozRequestFullScreen();
+        } else if (player.msRequestFullscreen) {
+            player.msRequestFullscreen();
+        } else {
+            console.error('Unable to find a fullscreen request method');
+        }
+    }
+}
+
+function toggleFullScreenClasses() {
+    player.classList.toggle('fullscreen')
+    isFullScreen = !isFullScreen
+}
 /* 
 hook up the event listeners 
 */
@@ -51,6 +88,7 @@ video.addEventListener('click', togglePlay)
 video.addEventListener('play', updateButton)
 video.addEventListener('play', updateButton)
 video.addEventListener('timeupdate', handleProgress)
+video.addEventListener('click', fullScreen)
 toggle.addEventListener('click', togglePlay)
 
 skipButtons.forEach(button => button.addEventListener
@@ -63,3 +101,11 @@ progress.addEventListener('click', scrub)
 progress.addEventListener('mousemove', () => mousedown && scrub(e))
 progress.addEventListener('mousedown', () => mousedown = true)
 progress.addEventListener('mouseup', () => mousedown = false)
+
+// full screen
+fullscreen.addEventListener('click', toggleFullScreen);
+
+document.addEventListener('fullscreenchange', toggleFullScreenClasses);
+document.addEventListener('mozfullscreenchange', toggleFullScreenClasses);
+document.addEventListener('webkitfullscreenchange', toggleFullScreenClasses);
+document.addEventListener('msfullscreenchange', toggleFullScreenClasses);
